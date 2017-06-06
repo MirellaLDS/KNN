b2efc6b6d3795554c12ce1a3114369bdd7ccf8c0
Mirella Lins <mlds@cin.ufpe.br>
2017-06-06 16:50:49 -0300

    file

--- program/program/Form1.Designer.cs
+++ program/program/Form1.Designer.cs
@@ -28,20 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(652, 22);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 11;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtFile.Enabled = false;
+            this.txtFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.txtFile.Location = new System.Drawing.Point(234, 23);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(412, 20);
+            this.txtFile.TabIndex = 10;
+            this.txtFile.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Path to classify Customer\'s file";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 262);
+            this.ClientSize = new System.Drawing.Size(887, 334);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- program/program/Form1.cs
+++ program/program/Form1.cs
@@ -12,9 +12,22 @@ namespace program
 {
     public partial class Form1 : Form
     {
+        string file; //file to classify
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog cFile = new OpenFileDialog();//open file windows
+            cFile.Filter = "classify customer file|*.txt";//filter only open .txt file
+
+            cFile.ShowDialog();
+
+            file = cFile.FileName;
+            this.txtFile.Text = cFile.FileName;//get path file
+
         }
     }
 }
